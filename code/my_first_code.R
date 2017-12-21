dafa6810df704486db7885e61fc370ec654d4340
Hena <hena.ramay@gmail.com>
2017-12-20 22:42:03 -0700

    added tidyverse

---
+++ code/my_first_code.R
@@ -0,0 +1,22 @@
+### This is a first code file for RbootCamp2018
+
+#load packages
+library(ggplot2)
+library(dplyr)
+
+
+#variable assignment
+a = 25
+b <-25
+c<- a+b
+print(c)
+
+#define arrays
+
+d=c(1,2,3,4,5,6,7)
+e=8:14
+f="Myplot"
+
+#plot example
+plot(d,e,main=f)
+
